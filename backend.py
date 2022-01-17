17812dceb420335644f4f557039d9cb1aecd24cb
SRCinVA <scitarella@hotmail.com>
2022-01-16 22:26:45 -0500

    Minor changes

--- backend.py
+++ backend.py
@@ -17,31 +17,31 @@ class Database:
     def insert(self, title, author, year, isbn):
         self.cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title,author,year,isbn))  # NULL because python creates that ID automatically. Still need to add all parameters a second time as a tuple.
         self.conn.commit()                                                                       
-        self. conn.close()
+        # self. conn.close()
 
     def view(self): # fetches all rows
         self.cur.execute("SELECT * FROM book")
         # conn.commit() no need to commit any changes                                                                      
-        rows=self.cur.fetchall()
-        self.conn.close() 
+        rows=self.cur.fetchall()  # 'rows' is a local variable that needs no self prefix
+        # self.conn.close() closing the db causes an error. Odd--why wasn't this an issue before?
         return rows
 
     def search(self, title="", author="", year="", isbn=""):  # need to pass the id of the row to delete
         self.cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn)) #still unclear why you need to pass in this tuple
-        rows=self.cur.fetchall()
-        self.conn.close() 
+        rows=self.cur.fetchall() # For some reason he prefixed 'rows' here with 'self'.
+        # self.conn.close() 
         return rows
 
     def delete(self,id):
         self.cur.execute("DELETE FROM book WHERE id=?", (id,)) # first is the "column name", then the "function parameter".
         # rows=cur.fetchall() won't need this one.
         self.conn.commit()
-        self.conn.close()
+        # self.conn.close()
 
     def update(self,id,title, author, year, isbn):
         self.cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id)) # column name" for id, then the "function parameter" for id.
         self.conn.commit()
-        self.conn.close()
+        # self.conn.close()
 
     # connect() def __init__() now takes care of this.
 
